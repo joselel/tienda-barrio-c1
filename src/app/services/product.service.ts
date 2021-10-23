ce77cb9331dd6db1e1b92bd1192b406d1acbea9c
Jose Elel <joselel2011@gmail.com>
2021-10-23 14:11:05 -0600

    limpieza-comentarios

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,7 +9,6 @@ export class ProductService {
 
   constructor(private firestore: AngularFirestore) { }
 
-
   getProducts(): Observable<any> {
     return this.firestore.collection('products', ref => ref.orderBy('admission_date', 'asc')).snapshotChanges();
   }
@@ -17,22 +16,4 @@ export class ProductService {
   getProduct(id: string): Observable<any> {
     return this.firestore.collection('empleados').doc(id).snapshotChanges();
   }
-
 }
-/**
-   * agregarEmpleado(empleado: any): Promise<any> {
-    return this.firestore.collection('empleados').add(empleado);
-  }
-
-  eliminarEmpleado(id: string): Promise<any> {
-    return this.firestore.collection('empleados').doc(id).delete();
-  }
-
-  getEmpleado(id: string): Observable<any> {
-    return this.firestore.collection('empleados').doc(id).snapshotChanges();
-  }
-
-  actualizarEmpleado(id: string, data:any): Promise<any> {
-    return this.firestore.collection('empleados').doc(id).update(data);
-  }
-   */
